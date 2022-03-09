3a43d9cbcfd9ab45db3c2fbb64eccc2b7cfce083
tluu5 <tluu5@uic.edu>
2022-03-09 13:52:29 -0600

    changes

---
+++ How_long_did_that_take.js
@@ -0,0 +1,35 @@
+// iterative
+function iFib(n) {
+    const vals = [0, 1];
+    while (vals.length - 1 < n) {
+        let len = vals.length;
+        vals.push(vals[len - 1] + vals[len - 2]);
+    }
+    return vals[n];
+}
+iFib(20);
+
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const reversed1 = story.split("").reverse().join("");
+
+Number.prototype.isPrime = function () {
+    for (let i = 2; i < this; i++) {
+        if (this % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while (primeCount < 1e4) {
+    if (num.isPrime()) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num - 1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
