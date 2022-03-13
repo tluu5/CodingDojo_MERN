1a720d32410906e876a68f7c64619633bc22cc06
tluu5 <tluu5@uic.edu>
2022-03-12 20:33:02 -0600

    changes

--- REACT/my-first-project/src/components/MyNewComponent.js
+++ REACT/my-first-project/src/components/MyNewComponent.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class PersonComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    changeAge = () => {
+        this.setState({ age: this.state.age + 1 });
+    };
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={this.changeAge}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
