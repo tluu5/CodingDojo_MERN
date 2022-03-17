a93d5a94141c7643240104e58661c7114717280a
tluu5 <tluu5@uic.edu>
2022-03-17 10:48:25 -0500

    changes

--- REACT/my-third-project/src/App.js
+++ REACT/my-third-project/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><marquee>Hey it's me!</marquee></h1>
+      <Counter text="hey I'm the component here!"/>
+
+      <Counter text="clickth you button!"/>
+
+      <Counter text="another component!"/>
     </div>
   );
 }
