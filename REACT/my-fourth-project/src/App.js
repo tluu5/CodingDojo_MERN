3985933c4c5424c19619583a9bbee9af5bba2fb6
tluu5 <tluu5@uic.edu>
2022-03-17 17:00:37 -0500

    changes

---
+++ REACT/my-fourth-project/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Pixel from './components/Pixel';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hey it's me!</h1>
+      <Pixel/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/my-fourth-project/src/components/Pixel.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+class Pixel extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            filled: false
+        }
+    }
+    onClickHandler = () => {
+        this.setState({
+            filled: !this.state.filled
+        })
+    }
+    render(){
+        return(
+            <div onClick={this.onClickHandler} className={this.state.filled ? "pixel filled" : "pixel"}></div>
+        )
+    }
+}
+export default Pixel;
