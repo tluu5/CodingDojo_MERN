309f0df38fa86bfd02e1843a3a908121caed86f7
tluu5 <tluu5@uic.edu>
2022-03-21 23:23:29 -0500

    changes

--- navbar_context/src/index.js
+++ navbar_context/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Wrapper from "./components/Wrapper";
+import NavBar from "./components/NavBar";
+import FormWrapper from "./components/FormWrapper";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import "./styles.css";
+
+function App() {
+  return (
+    <Wrapper>
+      <NavBar />
+      <FormWrapper />
+    </Wrapper>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
