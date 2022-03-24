978de1f67c84d70b498135481562cd328c781e1c
tluu5 <tluu5@uic.edu>
2022-03-24 15:55:06 -0500

    changes

--- REACT/my-tenth-project/src/App.js
+++ REACT/my-tenth-project/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import PokeCard from './components/PokeCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hey it's me!</h1>
+      <PokeCard pokemon="pikachu"/>
     </div>
   );
 }
---
+++ REACT/my-tenth-project/src/components/PokeCard.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+const PokeCard = (props) => {
+    const [defaultURL, setDefaultURL] = useState("");
+    const [shinyURL, setShinyURL] = useState("");
+    const [hover, setHover] = useState(false);
+    useEffect(()=>{
+        fetch("https://pokeapi.co/api/v2/pokemon/" + props.pokemon)
+            .then(res=>res.json())
+            .then(res=>{
+                console.log(res);
+                setDefaultURL(res.sprites.front_default);
+                setShinyURL(res.sprites.front_shiny);
+            })
+            .catch(err=>console.log(err))
+    },[])
+    const flipHover = () => setHover(!hover);
+    return(
+        <div className="poke-card">
+            <img onMouseEnter={flipHover} onMouseLeave={flipHover} src={hover ? shinyURL : defaultURL} alt={props.pokemon}/>
+            <h2>{props.pokemon}</h2>
+        </div>
+    )
+}
+
+export default PokeCard;
