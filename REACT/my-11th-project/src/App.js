8ff781a7d09e056062d525395a5b7bcebee23a54
tluu5 <tluu5@uic.edu>
2022-03-25 09:12:23 -0500

    changes

---
+++ REACT/my-11th-project/src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Home from './views/Home';
+import Second from './views/Second';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hey it's me!</h1>
+      <Home/>
+      <Second/>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/my-11th-project/src/views/Second.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+    return(
+        <div>
+            <h1><marquee>second page!!</marquee></h1>
+        </div>
+    )
+}
+
+export default Second;
