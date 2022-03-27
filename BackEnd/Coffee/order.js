31987ce43e835ee78cadf6790c9e9ba61aaf4805
tluu5 <tluu5@uic.edu>
2022-03-27 13:03:55 -0500

    changes

---
+++ BackEnd/Coffee/order.js
@@ -0,0 +1,18 @@
+const { faker } = require('@faker-js/faker');
+
+class Order{
+    constructor(){
+        const bases = ["espresso shot", "creamer", "ice", "dark roast", "light roast", "chai tea", "drip", "colombian", "black tea", "green tea", "hot water"];
+        const additives = ["vanilla syrup", "sugar", "splenda", "scoop of ice cream", "bricks", "pre workout", "jameson", "crinnamon", "honey", "more water", "milk", "chocolate syrup", "hot fudge", "pumpkin spice", "coca cola"];
+        const toppings = ["whipped cream", "nothing", "spinkled cinnamon", "caramel drizzle", "marshmallows", "shoe"]
+        this.name = faker.name.findName();
+        this.base = bases[Math.floor(Math.random() * bases.length)];
+        this.additive = additives[Math.floor(Math.random() * additives.length)];
+        this.coffeeShop = faker.company.companyName() + " Coffee Roaster Inc.";
+        this.cost = "$" + faker.finance.amount();
+        this.barista = faker.image.avatar();
+        this.topping = toppings[Math.floor(Math.random() * toppings.length)];
+    }
+}
+
+module.exports = Order;
--- BackEnd/Coffee/server.js
+++ BackEnd/Coffee/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 4000;
+const { faker } = require('@faker-js/faker');
+const Order = require("./order")
 
-app.listen(port, ()=>console.log(`running on port ${port}, hell yeah!`));
+app.get("/api", (req,res)=>{
+    res.json({message:"you found the secret route!"});
+})
+
+app.get("/api/fake", (req,res)=>{
+    const response = {
+        name: faker.name.findName(),
+        catchPhrase: faker.hacker.phrase()
+    }
+    res.json(response);
+})
+
+app.get("/api/order", (req,res)=>{
+    res.json(new Order());
+})
+
+app.listen(port, ()=>console.log(`Running on port ${port} is a new way I like to be!`));
