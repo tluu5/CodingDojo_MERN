f9fdac046e35c4884ac39d3d91147ddcd2f89e1f
tluu5 <tluu5@uic.edu>
2022-03-26 19:52:36 -0500

    changes

--- REACT/my-12th-project/src/components/Form.js
+++ REACT/my-12th-project/src/components/Form.js
@@ -2,10 +2,16 @@ import React, { useContext } from 'react';
 import WordContext from '../contexts/WordContext';
 
 const Form = (props) => {
-    const {form,setForm} = useContext(WordContext);
+    const {form,setForm, guesses, setGuesses} = useContext(WordContext);
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+        setGuesses([...guesses, form]);
+        setForm("");
+    }
     return(
-        <form className='w-50 mx-auto'>
-            <input value={form} onChange={(event) => setForm(event.target.value)} type="text" className='w-100 form-control'/>
+        <form onSubmit={onSubmitHandler} className='w-25 mx-auto m-5'>
+            <input value={form} onChange={(event) => setForm(event.target.value)} type="text" className='w-100 form-control' placeholder="what's your guess?"/>
+            <input type="submit" className='btn btn-info d-block mx-auto my-3' />
         </form>
     )
 }
